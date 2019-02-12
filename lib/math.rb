b205226f3c438d88e2a9235dd19371aa231bfd68
Michael Mottola <mottola64@gmail.com>
2019-02-12 02:18:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,27 @@
 def addition(num1, num2)
-puts 1+2
+puts num1 + num2
 end
 
 def subtraction(num1, num2)
-puts 2-1
+puts num1 - num2
 end
 
 def division(num1, num2)
-puts 1/2
+puts num1/num2
 end
 
 def multiplication(num1, num2)
+puts num1*num2
 end
 
 def modulo(num1, num2)
+puts num1 % num2
 end
 
 def square_root(num)
+puts Math.sqrt(num1)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+puts num1 + (( num2 * num3 ) / num4 )
 end
